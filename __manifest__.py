6f579e2366635607e931e148900c12e679e296b7
RAKIB6348 <shahidulhuda0@gmail.com>
2025-05-28 23:20:31 +0600

    create manifest

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name' : 'Real Estate',
+    'version' : '1.0.0',
+    'description' : 'This is a real estate module',
+    'summary' : 'This is a real estate module',
+    'license' : 'LGPL-3',
+    'depends' : [],
+    'data' : [],
+    'installable' : True,
+    'auto_install' : False,
+    'application' : True,
+}
